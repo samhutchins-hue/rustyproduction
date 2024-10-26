a23309c4ba5813192a8224ee027c4ba2c883463e
sam <samhutchins123@gmail.com>
2024-10-26 13:24:58 -0700

    updated run
    -removed async header
    -this supports the handling of GET to terminate
    -allows multiple iterations of testing

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,27 @@
-use zero2prod::main;
 
-#[test]
-fn dummy_test() {
-    main()
+#[tokio::test]
+async fn health_check_works() {
+    //Arrange
+    spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+// launch in background
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+    // launch server as a background task
+    // tokio spawn returns a handle to the spawned future,
+    // no use, so _
+    let _ = tokio::spawn(server);
 }
