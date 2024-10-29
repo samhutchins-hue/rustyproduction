e709795961bc05738c2e467fe667071c3cbfbc92
sam <samhutchins123@gmail.com>
2024-10-29 16:28:29 -0700

    added subscriptions route

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -42,7 +42,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     //Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
-        .post(&format!("{}/subscriptions", &app_address))
+        .post(format!("{}/subscriptions", app_address))
         .header("Content_Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
@@ -67,7 +67,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     for (invalid_body, error_message) in test_cases {
         //Act
         let response = client
-            .post(&format!("{}/subscriptions", &app_address))
+            .post(format!("{}/subscriptions", app_address))
             .header("Content_Type", "application/x-www-form-urlencoded")
             .body(invalid_body)
             .send()
