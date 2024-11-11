f020b2fabe6cd4c031347e902ac98af81ff114cf
sam <samhutchins123@gmail.com>
2024-11-10 23:32:50 -0800

    deserialization
    added serde for deserialization of the html form data

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -42,8 +42,8 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     //Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
-        .post(format!("{}/subscriptions", app_address))
-        .header("Content_Type", "application/x-www-form-urlencoded")
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
         .await
@@ -67,8 +67,8 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     for (invalid_body, error_message) in test_cases {
         //Act
         let response = client
-            .post(format!("{}/subscriptions", app_address))
-            .header("Content_Type", "application/x-www-form-urlencoded")
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
             .body(invalid_body)
             .send()
             .await
